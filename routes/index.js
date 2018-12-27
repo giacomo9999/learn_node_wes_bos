e869fae4367c370a9bbcf7b1191c8692af3b2e7b
James Gary <gmarchesi888@gmail.com>
2018-12-27 15:59:59 -0500

    Video 07.

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const storeController = require("../controllers/storeController");
 
-router.get("/", (req, res) => {
-  res.render("hello", {
-    name: "Jim",
-    dog: req.query.dog
-  });
-});
+router.get("/", storeController.homePage);
 
 module.exports = router;
