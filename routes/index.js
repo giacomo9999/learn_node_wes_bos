80b1a316377bc0dbb4fdcad746eeb9cda22aca4e
James Gary <gmarchesi888@gmail.com>
2018-12-27 15:05:19 -0500

    Video 05 again.

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-// Do work here
 router.get("/", (req, res) => {
-  const jim = { name: "jim", age: 51, sexy: true };
-  // res.send("Hey! It works!");
-  // res.json(req.query);
   res.render("hello", {
     name: "Jim",
     dog: req.query.dog
   });
-});
-
-router.get("/reverse/:name", (req, res) => {
-  const reverse = [...req.params.name].reverse().join("");
-  res.send(reverse);
 });
 
 module.exports = router;
