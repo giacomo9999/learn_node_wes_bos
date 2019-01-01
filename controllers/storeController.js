bc6da5a9accc9faeba6de9961a6b1070ef756698
James Gary <gmarchesi888@gmail.com>
2019-01-01 15:46:31 -0500

    Video 10

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,3 +2,12 @@ exports.homePage = (req, res) => {
   console.log("Homepage loading...");
   res.render("hello", { name: req.name, title: "Jimbo" });
 };
+
+exports.addStore = (req, res) => {
+  res.render("editStore", { title: "Add Store" });
+};
+
+exports.createStore = (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+};
